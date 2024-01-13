afa868f235c756ddb647477dd55e1ce736cd44ec
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-01-13 16:32:46 -0600

    added about page with app info and icon

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,7 +9,19 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-      <p>tinkl is a bathroom-finder app that locates nearby gender-neutral and single-stall bathrooms.</p>
+        <p>
+          Nearly 60% of trans Americans have avoided using public bathrooms in
+          the last year.
+        </p>
+
+        <div id="need-to-pee">
+          <img src="https://cdn4.iconfinder.com/data/icons/person-actions/227/person-action-006-512.png" />
+        </div>
+        <h5>
+          tinkl is a bathroom-finder app that locates nearby gender-neutral and
+          single-stall bathrooms so that trans, nonbinary and gender
+          non-conforming people can pee in peace.
+        </h5>
       </div>
     </div>
   );
