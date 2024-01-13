b34cd14072295d895a9fd1a2f11e040df02b65a7
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-01-12 18:42:28 -0600

    added insertBathrooms function to refuge_api.saga file

--- src/redux/sagas/refuge_api.saga.js
+++ src/redux/sagas/refuge_api.saga.js
@@ -8,8 +8,6 @@ function* loadApiBathrooms(action){
         const response = yield axios({
           method: 'GET',
           url: `https://refugerestrooms.org/api/v1/restrooms/by_location?lat=44.977753&lng=-93.2650108&search=Minneapolis%2C+Minnesota%2C&per_page=${action.payload.perPage}&page=${action.payload.pageNumber}`
-    
-        //   url: `/api/?perPage=${action.payload.perPage}&pageNumber=${action.payload.pageNumber}`
         })
 // sets api bathrooms reducer with returned bathrooms
       yield put({
@@ -23,8 +21,19 @@ function* loadApiBathrooms(action){
     }
 }
 
+function* insertBathrooms(action){
+try {
+  console.log('insertBathrooms action.payload:', action.payload)
+  yield axios.post('/bathrooms', action.payload)
+} catch (error) {
+  console.log('Error with Saga insertBathrooms function: ', error)
+}
+}
+
 function* apiSearchSaga(){
-    yield takeLatest('SAGA/LOAD_BATHROOMS_FROM_API', loadApiBathrooms)
+    yield takeLatest('SAGA/LOAD_BATHROOMS_FROM_API', loadApiBathrooms);
+    yield takeLatest('SAGA/ADD_BATHROOMS_TO_DB', insertBathrooms);
+
 }
 
 export default apiSearchSaga;
