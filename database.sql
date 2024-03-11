16e80ce9ecac6286b4f97e117bbb097ac85a3f65
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-03-11 03:00:36 -0500

    edited database.sql file to add updated_at trigger functions

--- database.sql
+++ database.sql
@@ -1,10 +1,129 @@
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "restrooms";
+DROP TABLE IF EXISTS "comments";
+DROP TABLE IF EXISTS "comment_votes";
+DROP TABLE IF EXISTS "images";
+DROP TABLE IF EXISTS "restroom_votes";
+DROP TABLE IF EXISTS "restroom_visits";
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+DROP TRIGGER IF EXISTS "trigger_update_updated_at_restrooms" ON "restrooms";
+DROP TRIGGER IF EXISTS "trigger_update_updated_at_comments" ON "comments";
+DROP TRIGGER IF EXISTS "trigger_update_updated_at_restroom_votes" ON "restroom_votes";
+DROP FUNCTION IF EXISTS "update_updated_at_restrooms";
+
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR(100) UNIQUE NOT NULL,
+	"password" VARCHAR(100),
+	"is_admin" BOOLEAN DEFAULT FALSE,
+	"is_removed" BOOLEAN DEFAULT FALSE,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+SET TIMEZONE = 'America/Chicago'; 
+
+CREATE TABLE "restrooms" (
+	"id" SERIAL PRIMARY KEY,
+	"api_id" VARCHAR,
+	"name" VARCHAR,
+	"street" VARCHAR,
+	"city" VARCHAR,
+	"state" VARCHAR,
+	"accessible" BOOLEAN DEFAULT FALSE,
+	"unisex" BOOLEAN DEFAULT FALSE,
+	"directions" TEXT,
+	"latitude" FLOAT(8),
+	"longitude" FLOAT(8),
+	"created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"country" VARCHAR,
+	"changing_table" BOOLEAN DEFAULT FALSE,
+	"is_removed" BOOLEAN DEFAULT FALSE,
+	"is_single_stall" BOOLEAN DEFAULT FALSE,
+	"is_multi_stall" BOOLEAN DEFAULT FALSE,
+	"is_flagged" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "comments" (
+	"id" SERIAL PRIMARY KEY,
+	"content" text,
+	"restroom_id" INTEGER REFERENCES "restrooms" ON DELETE CASCADE,
+	"user_id"  INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"is_removed" BOOLEAN DEFAULT FALSE,
+	"is_flagged" BOOLEAN DEFAULT FALSE,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW().
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE "comment_votes" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"comment_id" INTEGER REFERENCES "comments" ON DELETE CASCADE,
+	"vote" TEXT,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE "images" (
+	"id" SERIAL PRIMARY KEY,
+	"source" VARCHAR,
+	"user_id" INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"restroom_id" INTEGER REFERENCES "restrooms" ON DELETE CASCADE,
+	"is_removed" BOOLEAN DEFAULT FALSE,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE "restroom_votes" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"restroom_id" INTEGER REFERENCES "restrooms" ON DELETE CASCADE,
+	"upvote" INTEGER,
+	"downvote" INTEGER,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE "restroom_visits" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"restroom_id" INTEGER REFERENCES "restrooms" ON DELETE CASCADE,
+	"inserted_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+-- function to update the restrooms updated_at column with a new timestamp of the current time upon being triggered
+CREATE OR REPLACE FUNCTION update_updated_at_restrooms()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF TG_TABLE_NAME = 'comments' THEN
+		UPDATE restrooms
+    	SET updated_at = now()
+    	WHERE id = NEW.restroom_id;
+    ELSIF TG_TABLE_NAME = 'restroom_votes' THEN
+		UPDATE restrooms
+    	SET updated_at = now()
+    	WHERE id = NEW.restroom_id;
+    END IF;
+    
+    RETURN NULL;
+END;
+$$ language 'plpgsql';
+
+-- Attaches trigger function to restrooms table (will trigger update if info in the restrooms table is updated)
+CREATE TRIGGER trigger_update_updated_at_restrooms
+AFTER UPDATE ON restrooms
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_restrooms();
+
+-- Attaches trigger function to comments table (will trigger update if a new comment is inserted)
+CREATE TRIGGER trigger_update_updated_at_comments
+AFTER INSERT ON comments
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_restrooms();
+
+-- Attaches trigger function to restroom_votes table (will trigger update if a new vote is posted/inserted)
+CREATE TRIGGER trigger_update_updated_at_restroom_votes
+AFTER INSERT ON restroom_votes
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_restrooms();
