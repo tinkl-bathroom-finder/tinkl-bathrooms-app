52fc5c2b72246e82c3624652247ed7debaebd846
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-06-04 15:50:50 -0500

    added opening hours to bathroomsByDistance router

--- server/routes/location.router.js
+++ server/routes/location.router.js
@@ -1,23 +1,38 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /* GET route to get bathrooms by proximity to current location */
-router.get('/', (req, res) => {
-  console.log('req.query.lat:', req.query.lat)
-  console.log('req.query.lng:', req.query.lng)
+router.get("/", (req, res) => {
+  console.log("req.query.lat:", req.query.lat);
+  console.log("req.query.lng:", req.query.lng);
   // This uses the Haversine formula to calculate distances between coordinates. $1 is current location latitude, $2 is current location longitude.
-  const query = /*sql*/`
-	    select 
-      id, 
-      name, 
-      street, 
-      city, 
-      created_at, 
-      updated_at, 
-      accessible, 
-      unisex, 
-      changing_table,
+  const query = /*sql*/ `
+  	    SELECT 
+      "restrooms".id, 
+      "restrooms".name, 
+      "restrooms".street, 
+      "restrooms".city, 
+      "restrooms".created_at, 
+      "restrooms".updated_at, 
+      "restrooms".accessible, 
+      "restrooms".unisex, 
+      "restrooms".changing_table,
+   "opening_hours".weekday_text,
+   "opening_hours".day_0_open,
+   "opening_hours".day_0_close,
+   "opening_hours".day_1_open,
+   "opening_hours".day_1_close,
+   "opening_hours".day_2_open,
+   "opening_hours".day_2_close,
+   "opening_hours".day_3_open,
+   "opening_hours".day_3_close,
+   "opening_hours".day_4_open,
+   "opening_hours".day_4_close,
+   "opening_hours".day_5_open,
+   "opening_hours".day_5_close,
+   "opening_hours".day_6_open,
+   "opening_hours".day_6_close,
       COALESCE("votes_query"."upvotes", 0) AS "upvotes", 
       COALESCE("votes_query"."downvotes", 0) AS "downvotes",
           (
@@ -37,25 +52,44 @@ router.get('/', (req, res) => {
             GROUP BY "restroom_id"
           ) 
           AS "votes_query" ON "restrooms"."id" = "votes_query"."restroom_id"
+      LEFT JOIN "opening_hours" ON "restrooms".id="opening_hours".restroom_id
           WHERE "restrooms".is_removed = FALSE
+   GROUP BY "restrooms".id, "opening_hours".weekday_text, 
+   "opening_hours".day_0_open,
+   "opening_hours".day_0_close,
+   "opening_hours".day_1_open,
+   "opening_hours".day_1_close,
+   "opening_hours".day_2_open,
+   "opening_hours".day_2_close,
+   "opening_hours".day_3_open,
+   "opening_hours".day_3_close,
+   "opening_hours".day_4_open,
+   "opening_hours".day_4_close,
+   "opening_hours".day_5_open,
+   "opening_hours".day_5_close,
+   "opening_hours".day_6_open,
+   "opening_hours".day_6_close,
+   "votes_query".upvotes,
+   "votes_query".downvotes
   ORDER BY
     distance ASC
   LIMIT 200;
-    `
-  const lat = req.query.lat
-  const lng = req.query.lng
-  const values = [lat, lng]
+    `;
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  const values = [lat, lng];
 
-  pool.query(query, values)
+  pool
+    .query(query, values)
     .then((dbRes) => {
-      let bathroomsByDistanceList = dbRes.rows
+      let bathroomsByDistanceList = dbRes.rows;
       // gets sent to bathroomsByDistance reducer ==> action.payload
-      res.send(bathroomsByDistanceList)
+      res.send(bathroomsByDistanceList);
     })
     .catch((dbErr) => {
-      console.log('fail:', dbErr)
-      res.sendStatus(500)
-    })
-})
+      console.log("fail:", dbErr);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
