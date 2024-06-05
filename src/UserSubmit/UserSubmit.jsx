49c8c387dcbe372fb09030bf2f4e31354d6b9a35
RileyAlexis <MxRileyAlexis@gmail.com>
2024-06-05 12:39:24 -0500

    refactored map.jsx

---
+++ src/UserSubmit/UserSubmit.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export const UserSubmit = () => {
+    return (
+        <>
+        </>
+    );
+}
