f68c75ca317d53dadde2ec81907a44553c4ea999
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-07-19 23:07:39 -0500

    styling for landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,7 +15,7 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      <h2>tinkl</h2>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
@@ -31,15 +31,8 @@ function LandingPage() {
             More info coming soon!
           </p>
         </div>
-        <div className="grid-col grid-col_4">
+        <div className="grid-col grid-col_1">
           <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
         </div>
       </div>
     </div>
