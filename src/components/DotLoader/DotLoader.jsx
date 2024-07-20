2841c742141b82b0837c8f9cd921cab8094c95e7
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-07-19 23:05:39 -0500

    deleted dot sensor heading

--- src/components/DotLoader/DotLoader.jsx
+++ src/components/DotLoader/DotLoader.jsx
@@ -4,7 +4,6 @@ import { DotLoader } from 'react-spinners';
 function DotSensor() {
     return (
         <div>
-            <h3>Dot Sensor</h3>
             <div className='sweet-loading'>
                 <DotLoader color={'#5272F2'} loading={true} />
             </div>
