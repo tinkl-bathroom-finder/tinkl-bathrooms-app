90245788722414e99b25467360e0e7638c2bcfdb
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-07-23 01:05:56 -0500

    display open hours on bathroom Info Window card

--- src/components/BathroomItem/BathroomItemMap.jsx
+++ src/components/BathroomItem/BathroomItemMap.jsx
@@ -69,6 +69,17 @@ function BathroomItemMap({ bathroom, origin }) {
     return stringifiedDate;
   };
 
+  const convertToTwelveHourTime = (time) => {
+    let twelveHourTime = time/100
+    let amOrPm = "am" 
+    if (time > 1200) {
+      twelveHourTime = twelveHourTime - 12
+      amOrPm = "pm"
+    
+    }
+    return `${twelveHourTime}${amOrPm}`
+  }
+
   // console.log('bathroom: ', bathroom)
 
   return (
@@ -95,6 +106,7 @@ function BathroomItemMap({ bathroom, origin }) {
             subheader={bathroom.street}
             action={
               <>
+
                 {/* icons to show if bathrooms is all-gender, has changing table, is wheelchair accessible */}
                 <Typography variant="h6" align="right" sx={{ mr: 1, mt: 0.5 }}>
                   {bathroom.unisex ? <TransgenderOutlinedIcon /> : ""}
@@ -127,6 +139,16 @@ function BathroomItemMap({ bathroom, origin }) {
             }
           />
           <CardContent sx={{ p: 0 }}>
+
+                <Typography sx={{fontWeight: 'bold'}}>Business hours:</Typography>
+                <Typography>Monday: {bathroom.day_1_open ? bathroom.day_1_open + '-' + bathroom.day_1_close : "Closed"}</Typography>
+                <Typography>Tuesday: {bathroom.day_2_open ? convertToTwelveHourTime(bathroom.day_2_open) + '-' + convertToTwelveHourTime(bathroom.day_2_close) : "Closed"}</Typography>
+                <Typography>Wednesday: {bathroom.day_3_open ? convertToTwelveHourTime(bathroom.day_3_open ) + '-' + convertToTwelveHourTime(bathroom.day_3_close) : "Closed"}</Typography>
+                <Typography>Thursday: {bathroom.day_4_open ? convertToTwelveHourTime(bathroom.day_4_open ) + '-' + convertToTwelveHourTime(bathroom.day_4_close) : "Closed"}</Typography>
+                <Typography>Friday: {bathroom.day_5_open ? convertToTwelveHourTime(bathroom.day_5_open ) + '-' + convertToTwelveHourTime(bathroom.day_5_close) : "Closed"}</Typography>
+                <Typography>Saturday: {bathroom.day_6_open ? convertToTwelveHourTime(bathroom.day_6_open ) + '-' + convertToTwelveHourTime(bathroom.day_6_close) : "Closed"}</Typography>
+                <Typography>Sunday: {bathroom.day_0_open ? convertToTwelveHourTime(bathroom.day_0_open ) + '-' + convertToTwelveHourTime(bathroom.day_0_close) : "Closed"}</Typography>
+
             {/* distance from current/searched location */}
             <Typography
               align="right"
