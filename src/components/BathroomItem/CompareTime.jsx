37755adaf9863477d9a7b3d5bd1d8a23c984d395
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-08-20 12:50:14 -0500

    added logic to return isOpen as null if no business hours available in CompareTime.jsx

--- src/components/BathroomItem/CompareTime.jsx
+++ src/components/BathroomItem/CompareTime.jsx
@@ -2,9 +2,10 @@
 
 
 
-const CompareTime = (bathroom) => {
+const CompareTime = (props) => {
 
-    
+    let bathroom = props.bathroom;
+
     const date = new Date();
     let day = date.getDay(); // day comes back as number => 1 is Monday, 2 is Tuesday, etc.
     let hour = date.getHours() * 100; // formats hour as military time
@@ -14,20 +15,22 @@ const CompareTime = (bathroom) => {
    
     const bathroomStatus =()  => {
         let isOpen = false;
-    
-    if (day = 1 && militaryTime >= bathroom.bathroom.day_1_open && militaryTime <= bathroom.bathroom.day_1_close){
+        // if there are no opening hours listed for a business, set isOpen to null
+        if( !bathroom.day_1_open && !bathroom.day_2_open && !bathroom.day_3_open && !bathroom.day_4_open && !bathroom.day_5_open &&  !bathroom.day_6_open ){
+          isOpen = null;
+    } else if (day = 1 && militaryTime >= bathroom.day_1_open && militaryTime <= bathroom.day_1_close){
       isOpen = true;
-    }  else if (day = 2 && militaryTime >= bathroom.bathroom.day_2_open && militaryTime <= bathroom.bathroom.day_2_close){
+    }  else if (day = 2 && militaryTime >= bathroom.day_2_open && militaryTime <= bathroom.day_2_close){
       isOpen = true;
-    } else if (day = 3 && militaryTime >= bathroom.bathroom.day_3_open && militaryTime <= bathroom.bathroom.day_3_close){
+    } else if (day = 3 && militaryTime >= bathroom.day_3_open && militaryTime <= bathroom.day_3_close){
        isOpen = true;
-    } else if (day = 4 && militaryTime >= bathroom.bathroom.day_4_open && militaryTime <= bathroom.bathroom.day_4_close){
+    } else if (day = 4 && militaryTime >= bathroom.day_4_open && militaryTime <= bathroom.day_4_close){
       isOpen = true;
-    } else if (day = 5 && militaryTime >= bathroom.bathroom.day_5_open && militaryTime <= bathroom.bathroom.day_5_close){
+    } else if (day = 5 && militaryTime >= bathroom.day_5_open && militaryTime <= bathroom.day_5_close){
       isOpen = true;
-    } else if (day = 6 && militaryTime >= bathroom.bathroom.day_6_open && militaryTime <= bathroom.bathroom.day_6_close){
+    } else if (day = 6 && militaryTime >= bathroom.day_6_open && militaryTime <= bathroom.day_6_close){
       isOpen = true;
-    }  else if (day = 7 && militaryTime >= bathroom.bathroom.day_7_open && militaryTime <= bathroom.bathroom.day_7_close){
+    }  else if (day = 7 && militaryTime >= bathroom.day_7_open && militaryTime <= bathroom.day_7_close){
       isOpen = true;
     }  else{console.log('close')
     } return isOpen;
@@ -35,7 +38,13 @@ const CompareTime = (bathroom) => {
     let isOpenNow = bathroomStatus()
     console.log("open Now", bathroomStatus())
     return (
-        <h6 class={isOpenNow ? "open" : "closed"}> {isOpenNow ? 'Open Now' : 'Closed'}</h6>
+        <h6 class={isOpenNow ? "open" : "closed"}>
+          {/* if no business hours are available, nothing will display */}
+        { isOpenNow === true
+          ? 'Open Now' 
+          : isOpenNow === false
+          ? 'Closed'
+        : ''}</h6>
     )
    }
   
