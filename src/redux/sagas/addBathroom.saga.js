06d440e8301539899eef81829f9c436bdf3937cb
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-08-26 03:44:38 -0500

    add placeID params to addBathrooms saga, add clearPlaceDetails to same saga

--- src/redux/sagas/addBathroom.saga.js
+++ src/redux/sagas/addBathroom.saga.js
@@ -5,11 +5,14 @@ import axios from "axios";
 function* getPlaceDetails (action) {
     try {
         // makes a get request call to the Google Place Details API to get info for bathroom to add
-        const response = yield axios({
+        const response = yield axios.get({
           method: 'GET',
-          url: `/add/${action.payload}`
+          url: `/add/${action.payload}`,
+          params: {
+            placeID: action.payload
+          }
         })
-        // sets api bathrooms reducer with array of returned bathrooms
+        // sets new bathrooms reducer with details
         yield put({
           type: 'SET_NEW_BATHROOM_DETAILS',
           payload: response.data.results[0]
@@ -18,9 +21,21 @@ function* getPlaceDetails (action) {
         console.log('API get request failed:', error)
       }
 }
+
+function* clearPlaceDetails (action) {
+  try {
+      // clears new bathroom reducer
+      yield put({
+        type: 'CLEAR_NEW_BATHROOM_DETAILS'
+      })
+    } catch (error) {
+      console.log('Clear place details failed:', error)
+    }
+}
   
   function* addBathroom() { 
-    yield takeLatest("SAGA/GET_PLACE_DETAILS", getPlaceDetails);  
+    yield takeLatest("SAGA/GET_PLACE_DETAILS", getPlaceDetails);
+    yield takeLatest("SAGA/CLEAR_PLACE_DETAILS", clearPlaceDetails)  
   }
   
   export default addBathroom;
