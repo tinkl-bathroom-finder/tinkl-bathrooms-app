c62ac407b6ee5bccab22970b4866d2a006cf7006
Leo Johnson <leochinonejohnson@gmail.com>
2024-10-01 13:38:31 -0500

    built out skeleton for contact saga

---
+++ src/redux/sagas/contact.saga.js
@@ -0,0 +1,18 @@
+import { put, take, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* submitContact(action) {
+    try {
+        const response = yield axios({
+            method: 'POST',
+            url: '/contact',
+            payload: action.payload
+        })
+    } catch (error) {
+        console.log('Saga function fetchBathrooms failed: ', error)
+    }
+}
+
+export default function* contactSaga() {
+    yield takeLatest('SAGA/SUBMIT_CONTACT', fetchBathrooms);
+}
