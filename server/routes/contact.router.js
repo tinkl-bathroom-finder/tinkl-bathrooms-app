a54b41f6b087af2f22e64ca0cfaccd3c74de3e26
Leo Johnson <leochinonejohnson@gmail.com>
2024-10-01 15:41:05 -0500

    wrote get route for contacts

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -2,22 +2,31 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  pool.query(`SELECT * FROM "contact"`)
+    .then((dbRes) => {
+      res.send(dbRes.rows)
+    })
+    .catch((dbErr) => {
+      console.error('Contact get route failed', dbErr)
+      res.sendStatus(500)
+    })
+})
+
 router.post("/", (req, res) => {
-    const query = `
+  const query = `
     INSERT INTO "contact" (user_id, details)
     VALUES ($1, $2)
     `
-    const values = [req.user.id, req.body.feedback]
-
-    pool
-        .query (query, values)
-        .then((dbRes) => {
-            res.sendStatus(201)
-        })
-        .catch((dbErr) => {
-            console.error('Contact post route failed:', dbErr)
-            res.sendStatus(500)
-        })
+  const values = [req.user.id, req.body.feedback]
+  pool.query(query, values)
+    .then((dbRes) => {
+      res.sendStatus(201)
+    })
+    .catch((dbErr) => {
+      console.error('Contact post route failed:', dbErr)
+      res.sendStatus(500)
+    })
 })
 
 module.exports = router
