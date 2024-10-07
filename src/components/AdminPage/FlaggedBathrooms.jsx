04588dca0b3d6d52212ad744aa3be1be30b396ad
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-10-07 03:19:17 -0500

    hard-coded example FlaggedRestroom for UX demo

--- src/components/AdminPage/FlaggedBathrooms.jsx
+++ src/components/AdminPage/FlaggedBathrooms.jsx
@@ -1,41 +1,64 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Button } from "@mui/material";
-import Form from "react-bootstrap/Form";
-import Table from "react-bootstrap/Table";
+import { Box, Button, Table, TableContainer, TableHead, TableBody, TableCell, TableRow } from "@mui/material";
 import DeleteBathroomItem from "../BathroomItem/DeleteBathroomItem";
+import {
+  AccessibleForwardOutlined,
+  BabyChangingStationOutlined,
+  ExpandMore,
+  Man4,
+  Place,
+  TransgenderOutlined,
+  ThumbDownOutlined,
+  ThumbUpOutlined,
+} from "@mui/icons-material";
 
 function FlaggedBathrooms() {
   const dispatch = useDispatch();
   const store = useSelector((store) => store);
   const bathrooms = useSelector((store) => store.bathrooms);
-  const getAllBathrooms = () => {
-    dispatch({
-      type: "SAGA/FETCH_BATHROOMS",
-    });
-  };
 
   return (
-    <Box sx={{mt: 10}}>
-    <input placeholder="Enter name, id" label="Filter by:"></input>
-      <Button onClick={getAllBathrooms} variant="contained">Load all bathrooms</Button>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
+    <Box>
+      <Table>
+        <TableHead>
+          <TableRow>
             <th></th>
             <th>Name</th>
             <th>Street</th>
             <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-
-          {bathrooms.map((bathroom) => (
-            <DeleteBathroomItem key={bathroom.id} bathroom={bathroom} />
-          ))}
-        </tbody>
-      </table>
+            <th>Amenities</th>
+            <th></th>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+      <TableRow>
+        <TableCell sx={{fontWeight: 'bold'}}>Current info:</TableCell>
+        <TableCell>Blue Moon Coffee Cafe</TableCell>
+        <TableCell>3822 Lake St</TableCell>
+        <TableCell>Minneapolis</TableCell>
+        <TableCell>
+        </TableCell>
+        <TableCell>
+          {/* <Button color="info" variant="contained" size="small">Review suggested edits</Button> */}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+        <TableCell sx={{fontWeight: 'bold', color: 'green'}}>Suggested changes:</TableCell>
+        <TableCell sx={{color: 'green'}}>Milkweed</TableCell>
+        <TableCell>3822 Lake St</TableCell>
+        <TableCell>Minneapolis</TableCell>
+        <TableCell sx={{color: 'green'}}>
+          <AccessibleForwardOutlined/>
+          <Man4/>
+          <TransgenderOutlined/>
+        </TableCell>
+        <TableCell>
+          <Button color="info" variant="contained" size="small">Review suggested edits</Button>
+          </TableCell>
+        </TableRow>
+        </TableBody>
+      </Table>
     </Box>
   );
 }
