ce788b5cc91b7e4013d5a371f019142c8b587a8c
Leo Johnson <leochinonejohnson@gmail.com>
2024-10-09 17:12:36 -0500

    finished up component

--- src/components/AdminPage/AdminComments.jsx
+++ src/components/AdminPage/AdminComments.jsx
@@ -7,47 +7,62 @@ import { useDispatch, useSelector } from "react-redux";
 function AdminComments() {
     const dispatch = useDispatch();
     const feedbackArray = useSelector((store) => store.userFeedback)
+    console.log(feedbackArray)
 
     useEffect(() => {
-        dispatch({
-            type: "SAGA/FETCH_USER_FEEDBACK",
-          });
+        dispatch({ type: "SAGA/FETCH_USER_FEEDBACK" });
     }, [])
 
-      // formats inserted_at timestamp as readable string
-  const stringifyDate = (timestamp) => {
-    const date = new Date(timestamp);
-    const options = { year: "numeric", month: "short", day: "numeric" };
-    const stringifiedDate = date.toLocaleDateString("en-us", options);
-    return stringifiedDate;
-  };
+    // formats inserted_at timestamp as readable string
+    const stringifyDate = (timestamp) => {
+        const date = new Date(timestamp);
+        const options = { year: "numeric", month: "short", day: "numeric" };
+        const stringifiedDate = date.toLocaleDateString("en-us", options);
+        return stringifiedDate;
+    };
+
+    const resolveComment = (commentId) => {
+        dispatch({
+            type: "SAGA/RESOLVE_COMMENT",
+            payload: { commentId }
+        })
+    }
+
     return (
         <Box>
             <TableContainer>
-            <Table responsive="m" overflow="scroll">
-                <TableHead>
+                <Table responsive="m" overflow="scroll">
+                    <TableHead>
 
-                <TableRow>
-                <TableCell>Username</TableCell>
-                <TableCell>Details</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Mark as resolved</TableCell>
-            </TableRow>
-                </TableHead>
-                <TableBody>
-                    {feedbackArray?.map((comment) => (
                         <TableRow>
-                            <TableCell sx={{borderBottom: '1px solid darkgray'}}>{comment.username}</TableCell>
-                            <TableCell sx={{borderBottom: '1px solid darkgray'}}>{comment.details}</TableCell>
-                            <TableCell sx={{borderBottom: '1px solid darkgray'}}>{`${stringifyDate(comment.inserted_at)}`}</TableCell>
-                            <TableCell sx={{borderBottom: '1px solid darkgray'}}>
-                                <Button color="info" variant="contained" size="small">Mark as resolved</Button>
-                                </TableCell>
+                            <TableCell>Username</TableCell>
+                            <TableCell>Details</TableCell>
+                            <TableCell>Date</TableCell>
+                            <TableCell>Mark as resolved</TableCell>
                         </TableRow>
-                    ))
-                    }
-                </TableBody>
-            </Table>
+                    </TableHead>
+                    <TableBody>
+                        {feedbackArray?.map((comment) => (
+                            <TableRow key={comment.id}>
+                                <TableCell sx={{ borderBottom: '1px solid darkgray' }}>{comment.username}</TableCell>
+                                <TableCell sx={{ borderBottom: '1px solid darkgray' }}>{comment.details}</TableCell>
+                                <TableCell sx={{ borderBottom: '1px solid darkgray' }}>{`${stringifyDate(comment.inserted_at)}`}</TableCell>
+                                <TableCell sx={{ borderBottom: '1px solid darkgray' }}>
+                                    {comment.resolved === true ?
+                                        'Resolved!'
+                                        :
+                                        <Button color="info" variant="contained" size="small"
+                                            onClick={() => resolveComment(comment.comment_id)}
+                                        >
+                                            Mark as resolved
+                                        </Button>
+                                    }
+                                </TableCell>
+                            </TableRow>
+                        ))
+                        }
+                    </TableBody>
+                </Table>
             </TableContainer>
         </Box>
     )
