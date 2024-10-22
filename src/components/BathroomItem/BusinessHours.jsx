7a02081f7e8962f4ef37321aef58617e46f6dcdf
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2024-10-22 15:01:29 -0500

    adjusted business hours padding to match comments

--- src/components/BathroomItem/BusinessHours.jsx
+++ src/components/BathroomItem/BusinessHours.jsx
@@ -23,26 +23,25 @@ const BusinessHours = (props) => {
       twelveHourTime = 12
     }
     
-    // console.log('am/pm', amOrPm, time )
     return `${twelveHourTime}${amOrPm}`
   }
   if( bathroom.day_1_open || bathroom.day_2_open || bathroom.day_3_open || bathroom.day_4_open || bathroom.day_5_open ||  bathroom.day_6_open ){
     return (
-        <Accordion defaultExpanded>
+        <Accordion defaultExpanded sx={{pl: '16px'}}>
         <AccordionSummary 
         expandIcon={<ExpandMore />}
           aria-controls="panel1-content"
           id="panel1-header"
           >
-    <Typography sx={{fontWeight: 'bold'}}>Business hours:</Typography>
+    <Typography sx={{fontWeight: 'bold', ml: '-16px'}}>Business hours:</Typography>
     </AccordionSummary>
-    <Typography>Monday: {props.bathroom.day_1_open ? convertToTwelveHourTime(props.bathroom.day_1_open) + '-' + convertToTwelveHourTime(props.bathroom.day_1_close)  : "Closed"}</Typography>
-    <Typography>Tuesday: {props.bathroom.day_2_open ? convertToTwelveHourTime(props.bathroom.day_2_open) + '-' + convertToTwelveHourTime(props.bathroom.day_2_close) : "Closed"}</Typography>
-    <Typography>Wednesday: {props.bathroom.day_3_open ? convertToTwelveHourTime(props.bathroom.day_3_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_3_close) : "Closed"}</Typography>
-    <Typography>Thursday: {props.bathroom.day_4_open ? convertToTwelveHourTime(props.bathroom.day_4_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_4_close) : "Closed"}</Typography>
-    <Typography>Friday: {props.bathroom.day_5_open ? convertToTwelveHourTime(props.bathroom.day_5_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_5_close) : "Closed"}</Typography>
-    <Typography>Saturday: {props.bathroom.day_6_open ? convertToTwelveHourTime(props.bathroom.day_6_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_6_close) : "Closed"}</Typography>
-    <Typography>Sunday: {props.bathroom.day_0_open ? convertToTwelveHourTime(props.bathroom.day_0_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_0_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Monday: {props.bathroom.day_1_open ? convertToTwelveHourTime(props.bathroom.day_1_open) + '-' + convertToTwelveHourTime(props.bathroom.day_1_close)  : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Tuesday: {props.bathroom.day_2_open ? convertToTwelveHourTime(props.bathroom.day_2_open) + '-' + convertToTwelveHourTime(props.bathroom.day_2_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Wednesday: {props.bathroom.day_3_open ? convertToTwelveHourTime(props.bathroom.day_3_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_3_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Thursday: {props.bathroom.day_4_open ? convertToTwelveHourTime(props.bathroom.day_4_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_4_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Friday: {props.bathroom.day_5_open ? convertToTwelveHourTime(props.bathroom.day_5_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_5_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px'}}>Saturday: {props.bathroom.day_6_open ? convertToTwelveHourTime(props.bathroom.day_6_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_6_close) : "Closed"}</Typography>
+    <Typography variant='body1' sx={{pl: '8px', pb: '16px'}}>Sunday: {props.bathroom.day_0_open ? convertToTwelveHourTime(props.bathroom.day_0_open ) + '-' + convertToTwelveHourTime(props.bathroom.day_0_close) : "Closed"}</Typography>
     </Accordion >)
 }
 }
