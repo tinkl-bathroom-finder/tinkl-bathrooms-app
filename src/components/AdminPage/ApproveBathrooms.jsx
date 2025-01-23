60a2fd7619273fcd837af3586a383e6ef80ffd0b
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2025-01-22 23:57:36 -0600

    added "delete bathroom" function to admin dashboard page

--- src/components/AdminPage/ApproveBathrooms.jsx
+++ src/components/AdminPage/ApproveBathrooms.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Button, Table, TableContainer, TableHead, TableBody, TableCell, TableRow  } from "@mui/material";
+import { Box, Button, IconButton, Table, TableContainer, TableHead, TableBody, TableCell, TableRow  } from "@mui/material";
 import {
   AccessibleForwardOutlined,
   BabyChangingStationOutlined,
+  CheckCircle,
+  Delete,
   ExpandMore,
+  Edit,
   Man4,
   Place,
   TransgenderOutlined,
@@ -31,9 +34,32 @@ function ApproveBathrooms() {
     });
   }, []);
 
+  const deleteBathroomFromDatabase = (bathroom) => {
+    console.log("bathroom.id", bathroom.id);
+    // popup to confirm you are sure you want to delete
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      imageUrl: "https://media.giphy.com/media/LdgRBEyuJ6JNHsFEwF/giphy.gif",
+      imageWidth: 250,
+      imageHeight: 250,
+      imageAlt: "Pink poop",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, flush it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+    dispatch({
+      type: "SAGA/DELETE_BATHROOM",
+      payload: bathroom.id
+    })
+  }})
+  };
+
   return (
-    <Box>
-    <Table>
+    <Box sx={{backgroundColor: "#ffe6e8", m: 3, borderRadius: "8px"}}>
+    <Table sx={{textAlign: 'center'}}>
       <TableHead>
         <TableRow>
           <TableCell>Added by</TableCell>
@@ -42,8 +68,9 @@ function ApproveBathrooms() {
           <TableCell>Street</TableCell>
           <TableCell>City</TableCell>
           <TableCell>Amenities</TableCell>
-          <TableCell>Approve as is</TableCell>
-          <TableCell>Edit info</TableCell>
+          <TableCell>Delete</TableCell>
+          <TableCell>Edit</TableCell>
+          <TableCell>Approve</TableCell>
         </TableRow>
       </TableHead>
 
@@ -53,7 +80,7 @@ function ApproveBathrooms() {
         </TableRow> */}
         {bathroomsToApprove?.map((item) => (
       <TableRow>
-          <TableCell>{item.added_by_user}</TableCell>
+          <TableCell>{item.username}</TableCell>
           <TableCell>{`${stringifyDate(item.created_at)}`}</TableCell>
           <TableCell>{item.name}</TableCell>
           <TableCell>{item.street}</TableCell>
@@ -63,10 +90,19 @@ function ApproveBathrooms() {
             <TransgenderOutlined/>
           </TableCell>
           <TableCell>
-            <Button color="info" variant="contained" size="small">Approve as is</Button>
-            </TableCell>
+            <Button color="error" variant="contained" size="small" sx={ { borderRadius: 28 } } onClick={() => deleteBathroomFromDatabase(item)}>
+              <Delete/>
+              </Button>
+          </TableCell>
             <TableCell>
-              <Button color="error" variant="contained" size="small">Edit info</Button>
+              <Button color="primary" variant="contained" size="small" sx={ { borderRadius: 28 } }>
+                <Edit/>
+              </Button>
+            </TableCell>
+          <TableCell>
+            <Button color="success" variant="contained" size="small" sx={ { borderRadius: 28, color: 'white' } }>
+              <CheckCircle/>
+            </Button>
             </TableCell>
       </TableRow>
  ))} 
